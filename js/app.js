20633c6ef91a2ab8c2ae3bb692918795669f5318
nattyayana <natnalezayana@gmail.com>
2018-03-12 18:26:03 -0700

    finished all store inputs

--- js/app.js
+++ js/app.js
@@ -8,6 +8,8 @@
 // Done Display the values of each array as unordered lists in the browser
 
 
+// First and Pike Location.
+
 var storeHours= ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 var firstandpike = {
   storeLocation: '1st & Pike',
@@ -33,3 +35,120 @@ var firstandpike = {
 
 firstandpike.salePerHour();
 console.log(firstandpike);
+
+
+// SeaTac Airport Location.
+
+var seatacairport = {
+  storeLocation: 'SeaTac Airport',
+  projections:[],
+  minCust: 3,
+  maxCust: 24,
+  custAvgSale: 1.2,
+  salePerHour: function(){
+    var randomNumCustomer = ((Math.random() * (this.maxCust - this.minCust) + this.minCust))*this.custAvgSale;
+    var randomNum =  Math.round(randomNumCustomer);
+    this.projections.push(randomNum);
+
+    var seatacairportContainer = document.getElementById('seatacairport');
+
+    for( var i=0; 'i' < storeHours.length; i++ ) {
+      var teamElement = document.createElement('li');
+      teamElement.textContent = storeHours[i] + ':' + randomNum;
+      seatacairportContainer.appendChild(teamElement);
+    }
+
+  }
+};
+
+seatacairport.salePerHour();
+console.log(seatacairport);
+
+
+//Seattle Center Location.
+
+var seattlecenter = {
+  storeLocation: 'Seattle Center',
+  projections:[],
+  minCust: 11,
+  maxCust: 38,
+  custAvgSale: 3.7,
+  salePerHour: function(){
+    var randomNumCustomer = ((Math.random() * (this.maxCust - this.minCust) + this.minCust))*this.custAvgSale;
+    var randomNum =  Math.round(randomNumCustomer);
+    this.projections.push(randomNum);
+
+    var seattlecenterContainer = document.getElementById('seattlecenter');
+
+    for( var i=0; 'i' < storeHours.length; i++ ) {
+      var teamElement = document.createElement('li');
+      teamElement.textContent = storeHours[i] + ':' + randomNum;
+      seattlecenterContainer.appendChild(teamElement);
+    }
+
+  }
+};
+
+seattlecenter.salePerHour();
+console.log(seattlecenter);
+
+
+//Capitol Hill Location.
+
+var capitolhill = {
+  storeLocation: 'Capitol Hill',
+  projections:[],
+  minCust: 20,
+  maxCust: 38,
+  custAvgSale: 2.3,
+  salePerHour: function(){
+    var randomNumCustomer = ((Math.random() * (this.maxCust - this.minCust) + this.minCust))*this.custAvgSale;
+    var randomNum =  Math.round(randomNumCustomer);
+    this.projections.push(randomNum);
+
+    var capitolhillContainer = document.getElementById('capitolhill');
+
+    for( var i=0; 'i' < storeHours.length; i++ ) {
+      var teamElement = document.createElement('li');
+      teamElement.textContent = storeHours[i] + ':' + randomNum;
+      capitolhillContainer.appendChild(teamElement);
+    }
+
+  }
+};
+
+capitolhill.salePerHour();
+console.log(capitolhill);
+
+
+
+
+// Alki Store Location.
+
+
+var alki = {
+  storeLocation: 'Alki',
+  projections:[],
+  minCust: 2,
+  maxCust: 16,
+  custAvgSale: 4.6,
+  salePerHour: function(){
+    var randomNumCustomer = ((Math.random() * (this.maxCust - this.minCust) + this.minCust))*this.custAvgSale;
+    var randomNum =  Math.round(randomNumCustomer);
+    this.projections.push(randomNum);
+
+    var alkiContainer = document.getElementById('Alki');
+
+    for( var i=0; 'i' < storeHours.length; i++ ) {
+      var teamElement = document.createElement('li');
+      teamElement.textContent = storeHours[i] + ':' + randomNum;
+      alkiContainer.appendChild(teamElement);
+    }
+
+  }
+};
+
+alki.salePerHour();
+console.log(alki);
+
+
