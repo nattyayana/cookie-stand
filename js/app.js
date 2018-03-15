5378d5eb854a4f0836352a2e169365484d98f003
nattyayana <natnalezayana@gmail.com>
2018-03-14 23:30:37 -0700

    got buttons to the form set up going

--- js/app.js
+++ js/app.js
@@ -6,9 +6,13 @@ var allStores = [];
 
 var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm' , '8pm'];
 
+
 // We need to access the table that is in the DOM
 var storeTable= document.getElementById('store');
 
+
+// Access the form so we can attach an event listener
+var storeForm = document.getElementById('store-form');
 
 // We need a constructor to make our dog objects
 function Store(storeLocation, minCust, maxCust, avgCookieSale) {
@@ -122,3 +126,67 @@ var alki = new Store('Alki Beach', 2, 16, 4.6);
 alki.creatingCookieSales();
 alki.render();
 
+
+
+
+
+// UNCOMMENT BELLOW TO SEE WHERE I REALLY STOPPED AT
+
+
+
+// // It would be nice to have a single function that renders all of the individual dog rows...
+// function renderAllStores() {
+//   for(var i in allStores) {
+//     allStores[i].render();
+//   }
+// }
+
+
+// // Render the dogs in the table
+// Store.prototype.render = function () {
+//   var trElement = document.createElement('tr');
+
+//   var tdElement = document.createElement('td');
+//   tdElement.textContent = this.store;
+//   trElement.appendChild(tdElement);
+
+//   tdElement = document.createElement('td');
+//   tdElement.textContent = this.minCust;
+//   trElement.appendChild(tdElement);
+
+//   tdElement = document.createElement('td');
+//   tdElement.textContent = this.maxCust;
+//   trElement.appendChild(tdElement);
+
+//   tdElement = document.createElement('td');
+//   tdElement.textContent = this.hourlyCookieSales;
+//   trElement.appendChild(tdElement);
+
+//   storeTable.appendChild(trElement);
+// };
+
+
+
+// event listener's callback function
+function addNewStore(event){
+  event.preventDefault();
+  console.log(event.target.store.value);
+
+  var newStoreLocation = event.target.newStoreLocation.value;
+  var newMinCust = event.target.newMinCust.value;
+  var newMaxCust = event.target.newMaxCust.value;
+  var newAvgCookieSale = event.target.newAvgCookieSale.value;
+
+  new Store(newStoreLocation, newMinCust, newMaxCust, newAvgCookieSale);
+
+  storeTable.innerHTML = '';
+  makeHeaderRow();
+  renderAllStores();
+}
+
+// add event listener
+storeForm.addEventListener('submit', addNewStore);
+
+// Now we need to call our functions
+makeHeaderRow();
+renderAllStores();
